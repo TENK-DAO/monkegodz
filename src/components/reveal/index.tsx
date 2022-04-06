429b9e4026aa86d2eaa8b5258e623d56739719b6
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-06 15:31:39 -0400

    press escape or click anywhere to close reveal animation

--- src/components/reveal/index.tsx
+++ src/components/reveal/index.tsx
@@ -1,22 +1,22 @@
-import React, { useEffect, useState } from "react"
+import React, { useEffect, useMemo, useState } from "react"
 import Video from "../video"
 import { wallet } from "../../near"
 import useTenk from '../../hooks/useTenk'
 import * as css from "./reveal.module.css"
 
-const Wrap: React.FC<{ onClose: () => void }> = ({ onClose, children }) => (
-  <div className={css.reveal}>
-    {children}
-    <button onClick={onClose} title="Close">
-      &times;
-    </button>
-  </div>
-)
-
 const Reveal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const currentUser = wallet.getAccountId()
   const { stale, nfts } = useTenk()
   const [ended, setEnded] = useState(false)
+
+  const onKeyPress = useMemo(() => (e: KeyboardEvent) => {
+    if (e.key === 'Escape') onClose()
+  }, [])
+
+  useEffect(() => {
+    document.addEventListener('click', onClose)
+    document.addEventListener('keyup', onKeyPress)
+  }, [])
 
   useEffect(() => {
     if (!currentUser || (!stale && !nfts.length)) {
@@ -29,16 +29,14 @@ const Reveal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
   const nft = nfts[nfts.length - 1]
 
   return (
-    <Wrap onClose={onClose}>
+    <div className={css.reveal}>
       <Video
         src="RevealBackground.mp4"
         autoPlay
         loop
         style={{ display: ended ? undefined : 'none' }}
       />
-      <figure
-        style={{ display: ended ? undefined : 'none' }}
-      >
+      <figure style={{ display: ended ? undefined : 'none' }}>
         <img
           alt={nft.metadata?.title}
           src={nft.media}
@@ -54,7 +52,7 @@ const Reveal: React.FC<{ onClose: () => void }> = ({ onClose }) => {
           onEnded={() => setEnded(true)}
         />
       )}
-    </Wrap>
+    </div>
   )
 }
 
