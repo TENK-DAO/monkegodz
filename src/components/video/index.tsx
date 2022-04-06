dd61186a9115123bcd72c259ee17aa52d0d323dd
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-06 11:31:52 -0400

    feat: fix video autoplay in Chrome

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -11,7 +11,7 @@ type Props = React.DetailedHTMLProps<
 const Video: React.FC<Props> = ({ src, ...props }) => {
   const { videoH264, videoVP9 } = useVideoData(src)
   return (
-    <video playsInline preload="auto" {...props}>
+    <video playsInline muted preload="auto" {...props}>
       <source src={videoVP9.path} type="video/webm; codecs=vp9,opus" />
       <source src={videoH264.path} type="video/mp4; codecs=avc1" />
     </video>
