982518ae272b35e85e502d31db158cc98812acb8
Chad Ostrowski <221614+chadoh@users.noreply.github.com>
2022-04-06 12:05:04 -0400

    fix: rm VP9 video because Apple
    Apple and their horrible Safari desktop browser won't let us have nice
    things.
    
    The smaller-file-size VP9 looks all squased in Safari, but looks correct
    everywhere else.
    
    Support for h264 is near-universal, so even though it's a larger file
    size, we may as well go with just it.

--- src/components/video/index.tsx
+++ src/components/video/index.tsx
@@ -9,10 +9,9 @@ type Props = React.DetailedHTMLProps<
 }
 
 const Video: React.FC<Props> = ({ src, ...props }) => {
-  const { videoH264, videoVP9 } = useVideoData(src)
+  const { videoH264 } = useVideoData(src)
   return (
     <video playsInline muted preload="auto" {...props}>
-      <source src={videoVP9.path} type="video/webm; codecs=vp9,opus" />
       <source src={videoH264.path} type="video/mp4; codecs=avc1" />
     </video>
   )
